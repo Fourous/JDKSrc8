45d03a87db2c0290020fc9b17adf9a325cbb2fca
fourous <fourousky@163.com>
2022-08-22 15:11:31 +0800

    init project

---
+++ test/com/nio/ChatServer.java
@@ -0,0 +1,154 @@
+package com.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 使用JDK原生NIO实现的Chat逻辑，要掌握原生实现才能看懂Netty包装逻辑
+ *
+ */
+public class ChatServer {
+    public static void main(String[] args) throws IOException {
+        // 调用SelectorProvider.provider().openSelector() 通过平台选择合适的Selector
+        Selector selector = Selector.open();
+        // 创建ServerSocketChannel
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 绑定端口
+        serverSocketChannel.bind(new InetSocketAddress(9097));
+        // 设置非阻塞
+        serverSocketChannel.configureBlocking(false);
+        // 将accept事件绑定到selector上
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (true) {
+            // 阻塞在select上
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            // 遍历selectKeys
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey selectionKey = iterator.next();
+                // 如果是accept事件
+                if (selectionKey.isAcceptable()) {
+                    ServerSocketChannel ssc = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = ssc.accept();
+                    System.out.println("accept new conn: " + socketChannel.getRemoteAddress());
+                    socketChannel.configureBlocking(false);
+                    // 注意，这里是只进行了注册连接未发送消息，所以需要注册一个read事件
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    // 加入群聊
+                    ChatHolder.join(socketChannel);
+                } else if (selectionKey.isReadable()) {
+                    // 如果是读取事件
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+                    // 将数据读入到buffer中
+                    int length = socketChannel.read(buffer);
+                    if (length > 0) {
+                        buffer.flip();
+                        byte[] bytes = new byte[buffer.remaining()];
+                        // 将数据读入到byte数组中
+                        buffer.get(bytes);
+                        // 换行符会跟着消息一起传过来
+                        String content = new String(bytes, "UTF-8").replace("\r\n", "");
+                        if (content.equalsIgnoreCase("quit")) {
+                            // 退出群聊
+                            ChatHolder.quit(socketChannel);
+                            selectionKey.cancel();
+                            socketChannel.close();
+                        } else {
+                            // 扩散
+                            ChatHolder.propagate(socketChannel, content);
+                        }
+                    }
+                }
+                iterator.remove();
+            }
+        }
+    }
+
+    private static class ChatHolder {
+        private static final Map<SocketChannel, String> USER_MAP = new ConcurrentHashMap<>();
+
+        /**
+         * 加入群聊
+         * 每个链接就是一个socketChannel
+         * @param socketChannel
+         */
+        public static void join(SocketChannel socketChannel) {
+            // 有人加入就给他分配一个id
+            String userId = "用户" + ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+            send(socketChannel, "您的id为：" + userId + "\n\r");
+
+            for (SocketChannel channel : USER_MAP.keySet()) {
+                send(channel, userId + " 加入了群聊" + "\n\r");
+            }
+
+            // 将当前用户加入到map中
+            USER_MAP.put(socketChannel, userId);
+        }
+
+        /**
+         * 退出群聊
+         *
+         * @param socketChannel
+         */
+        public static void quit(SocketChannel socketChannel) {
+            String userId = USER_MAP.get(socketChannel);
+            send(socketChannel, "您退出了群聊" + "\n\r");
+            USER_MAP.remove(socketChannel);
+
+            for (SocketChannel channel : USER_MAP.keySet()) {
+                if (channel != socketChannel) {
+                    send(channel, userId + " 退出了群聊" + "\n\r");
+                }
+            }
+        }
+
+        /**
+         * 扩散说话的内容
+         *
+         * @param socketChannel
+         * @param content
+         */
+        public static void propagate(SocketChannel socketChannel, String content) {
+            String userId = USER_MAP.get(socketChannel);
+            for (SocketChannel channel : USER_MAP.keySet()) {
+                if (channel != socketChannel) {
+                    send(channel, userId + ": " + content + "\n\r");
+                }
+            }
+        }
+
+        /**
+         * 发送消息
+         *
+         * @param socketChannel
+         * @param msg
+         */
+        private static void send(SocketChannel socketChannel, String msg) {
+            try {
+                // ByteBuffer 分配内存空间-固定
+                ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+                // buffer写入 channel read 方法也可
+                writeBuffer.put(msg.getBytes());
+                // flip 切换为读模式
+                writeBuffer.flip();
+                // channel 写入 buffer read 方法也可
+                socketChannel.write(writeBuffer);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
